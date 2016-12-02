1204641863c39ab9c33bcecd0f56a65e22f695a0
Jamie Morton <jamietmorton@gmail.com>
2016-12-02 13:07:49 -0800

    Fixing import

--- q2_composition/__init__.py
+++ q2_composition/__init__.py
@@ -1,6 +1,6 @@
 from ._impute import add_pseudocount
 from ._ancom import ancom
-
+from .plugin_setup import Composition
 __version__ = '0.0.1'
 
-__all__ = ['add_pseudocount', 'ancom']
+__all__ = ['add_pseudocount', 'ancom', 'Composition']
